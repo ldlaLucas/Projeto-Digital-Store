ca1034129c5a80f062456c04aecf67b1a4a4a860
Lucas Augusto <ldlaconcursos@gmail.com>
2024-12-24 17:26:37 -0300

    Compontizacao de produtos

---
+++ src/components/ProductData/ProductData.jsx
@@ -0,0 +1,10 @@
+const products = [
+  { id: 1, name: 'Camiseta 1', image: 'public/product-thumb-1.jpeg', price: 49.9, category: 'camisas', description: 'Descrição da Camiseta 1' },
+  { id: 2, name: 'Calça 1', image: 'public/product-thumb-2.jpeg', price: 99.9, category: 'calcas', description: 'Descrição da Calça 1' },
+  { id: 3, name: 'Boné 1', image: 'public/product-thumb-3.jpeg', price: 29.9, category: 'bones', description: 'Descrição do Boné 1' },
+  { id: 4, name: 'Headphone 1', image: 'public/product-thumb-4.jpeg', price: 199.9, category: 'headphones', description: 'Descrição do Headphone 1' },
+  { id: 5, name: 'Tênis 1', image: 'public/product-thumb-5.jpeg', price: 149.9, category: 'tenis', description: 'Descrição do Tênis 1' },
+  // Adicione mais produtos conforme necessário
+];
+
+export default products;
