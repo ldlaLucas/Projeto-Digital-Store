6d57795ebcc283f06708a0db592b3c477495b256
Lucas Augusto <ldlaconcursos@gmail.com>
2024-12-24 18:26:32 -0300

    Limpeza do NotFound

--- src/Routes/Paths.jsx
+++ src/Routes/Paths.jsx
@@ -1,28 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import HomePage from '../pages/Home/HomePage';
-import ProductViewPage from '../pages/ProductViewPage/ProductViewPage';
-import OrdersPage from  '../pages/OrdersPage/OrdersPage';
-import ProductListingPage from '../pages/ProductListingPage/ProductListingPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import HomePage from "../pages/Home/HomePage";
+import ProductViewPage from "../pages/ProductViewPage/ProductViewPage";
+import OrdersPage from "../pages/OrdersPage/OrdersPage";
+import ProductListingPage from "../pages/ProductListingPage/ProductListingPage";
 import "./styles.css";
-
-const NotFound = () => {
-  const navigate = useNavigate();
-
-  return(
-    <div className='NotFound'>
-      <h1>404 PAGINA NÃO ENCONTRADA</h1>
-      <p>A pagina que esta procurando nao existe, contate o suporte ou volte para a pagina inicial no botao a baixo.</p>
-      <button className="NotFoundButton"
-      onClick={()=>navigate('/')}
-      onMouseEnter={(e) => (e.target.style.backgroundColor = "var(--dark-gray-2)")}
-        onMouseLeave={(e) => (e.target.style.backgroundColor = "var(--primary)")}
-      >
-        Voltar para Home
-      </button>
-    </div>
-  )
-}
+import CategoryPage from "../pages/CategoryPage/CategoryPage";
+import NotFound from "../pages/NotFound/NotFound";
 
 const Paths = () => {
   return (
@@ -32,6 +16,7 @@ const Paths = () => {
         <Route path="/product/:id" element={<ProductViewPage />} />
         <Route path="/products" element={<ProductListingPage />} />
         <Route path="/productsCart" element={<OrdersPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
