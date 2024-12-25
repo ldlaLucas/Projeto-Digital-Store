cee8990515c5d800ed389777d3b784f7f74baa0e
Lucas Augusto <ldlaconcursos@gmail.com>
2024-12-25 17:28:29 -0300

    atualizacao do NavBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 import Logo from "../Logo/Logo";
 import Search from "./Search";
 import NavBar from "./NavBar";
-import {useCart} from "../ShoppingCart/ShoppingCart";
+import { useCart } from "../ShoppingCart/ShoppingCart";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
@@ -13,18 +13,23 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-top">
-        <Logo type="header" />
+        <div className="logoNavBar">
+          <Logo type="header" className="LogoHeader" />
+          <NavBar />
+        </div>
         <Search />
         <div className="auth-buttons">
           <button className="register-button">Cadastrar</button>
           <button className="login-button">Entrar</button>
-          <div className="icon-with-indicator" onClick={() =>navigate("/orders")}>
+          <div
+            className="icon-with-indicator"
+            onClick={() => navigate("/orders")}
+          >
             <span className="indicator ">{cartItems.length}</span>
             <i className="icon pi pi-shopping-cart" />
           </div>
         </div>
       </div>
-      <NavBar />
     </header>
   );
 };
--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './styles.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav className="navigation">
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/products">Produtos</a></li>
-        <li><a href="/categories">Categorias</a></li>
-        <li><a href="/orders" >Meus Pedidos</a></li>
+        <li><NavLink to="/" activeClassName = "active">Home</NavLink></li>
+        <li><NavLink to="/products" activeClassName = "active">Produtos</NavLink></li>
+        <li><NavLink to="/categories" activeClassName = "active">Categorias</NavLink></li>
+        <li><NavLink to="/orders"  activeClassName = "active">Meus Pedidos</NavLink></li>
       </ul>
     </nav>
   );
