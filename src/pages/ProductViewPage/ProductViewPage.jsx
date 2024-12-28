e01cb6ea82d3c0ed0a950726c57ae736cc987b5f
Lucas Augusto <ldlaconcursos@gmail.com>
2024-12-28 18:10:58 -0300

    limpeza de comentarios

--- src/pages/ProductViewPage/ProductViewPage.jsx
+++ src/pages/ProductViewPage/ProductViewPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles.css";
 import Layout from "../../components/Layout/Layout";
-import ProductDetails from "../../components/ProductDetails/ProductDetails"; // Importar o componente ProductDetails
+import ProductDetails from "../../components/ProductDetails/ProductDetails";
 import ProductOptions from "../../components/ProductOptions/ProductOptions";
 import BuyBox from "../../components/BuyBox/BuyBox";
 import { useNavigate, useParams } from "react-router-dom";
 import products from "../../components/ProductData/ProductData";
-import ProductListing from "../../components/ProductListing/ProductListing"; // Importar o componente ProductListing
+import ProductListing from "../../components/ProductListing/ProductListing";
 
 const ProductViewPage = () => {
   const navigate = useNavigate();
@@ -27,7 +27,7 @@ const ProductViewPage = () => {
   }
 
   const handleBuyClick = () => {
-    navigate('/buy', { state: { product } });
+    navigate("/buy", { state: { product } });
   };
 
   const handleBackClick = () => {
@@ -38,41 +38,64 @@ const ProductViewPage = () => {
     <Layout>
       <div className="product-view-page">
         <div className="main-content">
-          {/* Galeria de Imagens */}
           <div className="gallery">
-            <img src={product.image} alt={product.name} className="main-image" />
+            <img
+              src={product.image}
+              alt={product.name}
+              className="main-image"
+            />
             <div className="thumbnails">
-              {/* Adicionar miniaturas de imagens aqui */}
-              <img src={product.image} alt={product.name} className="thumbnail" />
-              <img src={product.image} alt={product.name} className="thumbnail" />
-              <img src={product.image} alt={product.name} className="thumbnail" />
+              <img
+                src={product.image}
+                alt={product.name}
+                className="thumbnail"
+              />
+              <img
+                src={product.image}
+                alt={product.name}
+                className="thumbnail"
+              />
+              <img
+                src={product.image}
+                alt={product.name}
+                className="thumbnail"
+              />
             </div>
           </div>
-          {/* BuyBox */}
+
           <div className="buy-box">
             <h1>{product.name}</h1>
             <p>Referência: {product.reference}</p>
             <div className="rating">
-              {/* Adicionar estrelas e avaliação aqui */}
               <span>{product.stars} estrelas</span>
               <span>({product.rating} avaliações)</span>
             </div>
             <div className="price">
-              {product.priceDiscount && <p className="price-discount">R${product.priceDiscount}</p>}
+              {product.priceDiscount && (
+                <p className="price-discount">R${product.priceDiscount}</p>
+              )}
               <p className="price-original">R${product.price}</p>
             </div>
             <p>{product.description}</p>
-            {product.options && <ProductOptions options={product.options} />} {/* Verificar se options está definido */}
-            <BuyBox id={product.id} name={product.name} image={product.image} price={product.price} priceDiscount={product.priceDiscount} /> {/* Passar a prop name corretamente */}
-            <button onClick={handleBuyClick} className="button buy-button">COMPRAR</button>
+            {product.options && <ProductOptions options={product.options} />}
+
+            <BuyBox
+              id={product.id}
+              name={product.name}
+              image={product.image}
+              price={product.price}
+              priceDiscount={product.priceDiscount}
+            />
+
+            <button onClick={handleBuyClick} className="button buy-button">
+              COMPRAR
+            </button>
           </div>
         </div>
-        {/* Detalhes do Produto */}
-        <ProductDetails product={product} /> {/* Utilizar o componente ProductDetails */}
-        {/* Seção de Produtos Relacionados */}
+        <ProductDetails product={product} />
         <div className="related-products">
           <h2>Produtos Relacionados</h2>
-          <ProductListing products={products.slice(0, 4)} /> {/* Exibir os primeiros 4 produtos como relacionados */}
+          <ProductListing products={products.slice(0, 4)} />
         </div>
         <button onClick={handleBackClick} className="back-button">
           Página anterior
